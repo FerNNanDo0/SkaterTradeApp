230aae51435748a642256304e157125a0e7a949d
FerNNanDo0 <88053663+FerNNanDo0@users.noreply.github.com>
2023-09-12 15:16:21 -0300

    commit final 05

--- app/src/main/java/com/droid/app/skaterTrader/activity/LogoInicialActivity.java
+++ app/src/main/java/com/droid/app/skaterTrader/activity/LogoInicialActivity.java
@@ -8,15 +8,17 @@ import android.os.Handler;
 import com.droid.app.skaterTrader.R;
 public class LogoInicialActivity extends AppCompatActivity {
     private final int TIME = 3500;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_logo_inicial);
+
+        assert getSupportActionBar() != null;
+        getSupportActionBar().hide();
         new Handler().postDelayed(new Runnable() {
             @Override
-            public void run() {
-                startActivity( new Intent(getApplicationContext(), ActivityMain.class));
+            public void run() {startActivity( new Intent(getApplicationContext(), ActivityMain.class));
+
             }
         },TIME);
     }
